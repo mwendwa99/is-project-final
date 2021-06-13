4a29ef51a27ce5b8a3c6008e17c879467d343ae4
Brian Mwendwa <brianmwendwa1999@gmail.com>
2021-06-13 18:43:49 +0300

    add map

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -3,6 +3,7 @@ import { makeStyles, Grid, Paper, Typography } from '@material-ui/core';
 
 import { assets } from '../Assets/Index';
 import SearchBar from './SearchBar';
+import Map from './Map';
 
 const UseStyle = makeStyles((theme) => ({
     root: {
@@ -21,29 +22,32 @@ export default function SimpleContainer() {
     const classes = UseStyle()
 
     return (
-        <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            className={classes.root}
-        >
-            <Grid sm={5} item="sm" >
-                <img src={assets.map} alt="map-illustration" />
+        <div className='body__section' >
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                className={classes.root}
+            >
+                <Grid sm={5} item="sm" >
+                    <img src={assets.map} alt="map-illustration" />
+                </Grid>
+                <Grid sm={5} className={classes.containers} >
+                    <Grid item="xs" >
+                        <Typography variant='h1' >find a space in Nairobi, CBD</Typography>
+                    </Grid>
+                    <Grid item="xs" style={{ paddingBottom: "1rem" }}>
+                        <SearchBar />
+                    </Grid>
+                    <Grid item="xs" style={{ paddingTop: "5rem" }}>
+                        <Typography variant='body1' >Parking in the CBD has just been made easier! <br />
+                            Save-A-Spot is an application designed for motorists who need a parking space within the CBD.
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid sm={5} className={classes.containers} >
-                <Grid item="xs" >
-                    <Typography variant='h1' >find a space in Nairobi, CBD</Typography>
-                </Grid>
-                <Grid item="xs" style={{ paddingBottom: "1rem" }}>
-                    <SearchBar />
-                </Grid>
-                <Grid item="xs" style={{ paddingTop: "5rem" }}>
-                    <Typography variant='body1' >Parking in the CBD has just been made easier! <br />
-                    Save-A-Spot is an application designed for motorists who need a parking space within the CBD.
-                </Typography>
-                </Grid>
-            </Grid>
-        </Grid>
+            <Map />
+        </div>
     );
 }
---
+++ client/src/Components/Map.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Map = () => {
+    return (
+        <div className='map__section' >
+            <Typography variant="h1" >Spots found...</Typography>
+            <div className="map__section--frame">
+                <div className="map__section--mapouter">
+                    <div className="map__section--gmap_canvas">
+                        <iframe
+                            title="cbd map"
+                            width="870"
+                            height="340"
+                            id="gmap_canvas"
+                            src="https://maps.google.com/maps?q=nairobi%20cbd&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        </iframe>
+                        <a href="https://putlocker-is.org"></a>
+                        <br />
+                        <a href="https://www.embedgooglemap.net">build custom google map</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Map
