80ee85f3c42f47039c864d4e90c08dc15ffc7af2
Brian Mwendwa <brianmwendwa1999@gmail.com>
2021-08-07 14:18:20 +0300

    redo about

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid, Typography, Divider, makeStyles } from '@material-ui/core';
+import { Container, Grid, Typography, Divider, Paper, makeStyles } from '@material-ui/core';
 import { Email, GitHub, LinkedIn } from '@material-ui/icons';
 import Assets from '../Assets/Index'
 
@@ -7,10 +7,16 @@ const UseStyles = makeStyles((theme) => ({
     root: {
         // backgroundColor: "red",
         height: "100%",
-        padding: theme.spacing(1),
+        paddingTop: theme.spacing(1),
         display: "flex",
+        flexDirection: 'row',
         justifyContent: "center",
         // alignItems: 'center',
+    },
+    paper: {
+        backgroundColor: '#EDF5E0',
+        borderRadius: 10,
+        padding: theme.spacing(1)
     },
     aboutSection: {
         height: "100%",
@@ -32,46 +38,28 @@ const About = () => {
     return (
         <div className="body__section">
             <Container maxWidth='lg' className={classes.root}>
-                <Grid container style={{ flex: 1 }}>
-                    <div style={{ width: '100%' }}>
-                        <img width="100%" height='100%' src={Assets.busyStreet} alt="congestion" />
-                    </div>
-                </Grid>
-                {/* <div style={{ padding: 5 }}>
-                    <Divider style={{ backgroundColor: 'red' }} orientation='vertical' />
-                </div> */}
-                <Grid container className={classes.aboutSection}>
-                    <Grid item sm={12}>
-                        <div style={{}}>
-                            <Typography variant='h1' gutterBottom="true">About</Typography>
-                            <Typography variant='body2' gutterBottom='true'>
-                                The motivation behind Save A Spot is to reduce the hassle it takes for motorists
-                                to get parking space within the CBD as well as to reduce effort the NCCG takes to
-                                enforce the rules of parking in the CBD.<br /><br />
-                                Save A Spot presents an opportunity for motorists to conveniently secure a parking
-                                space within the Central Business District. With just a few clicks, a user can retrieve
-                                information about free spaces available, price of the allocated space and their booking
-                                information all through the internet.
-                            </Typography>
-                            <Typography variant='h1' gutterBottom='true'>Contacts</Typography>
-                            <Typography variant='body2' gutterBottom='true'>
-                                Have any suggestions or comments? Reach out to me!
-                            </Typography>
+                <Grid container>
+                    <Grid item md={6}>
+                        <div style={{ display: 'flex', justifyContent: 'center' }} >
+                            <img
+                                style={{ objectFit: 'contain' }}
+                                // width="100%" height="600px" 
+                                src={Assets.busyStreet} alt="" />
                         </div>
                     </Grid>
-                    <Grid container className={classes.contacts} >
-                        <Grid item>
-                            <Email />
-                            <Typography>brianmwendwa.mu@gmail.com</Typography>
-                        </Grid>
-                        <Grid item>
-                            <GitHub />
-                            <Typography>mwendwa99</Typography>
-                        </Grid>
-                        <Grid item>
-                            <LinkedIn />
-                            <Typography>Brian Mwendwa</Typography>
-                        </Grid>
+                    <Grid item md={6}>
+                        {/* <div > */}
+                        <Paper className={classes.paper}>
+                            <Typography variant='h1'>About</Typography>
+                            <Typography>The motivation behind Save A Spot is to reduce the hassle it takes for motorists to get parking space
+                                within the CBD as well as to reduce effort the NCCG takes to enforce the rules of parking in the CBD.
+                                <br />
+                                <br />
+                                Save A Spot presents an opportunity for motorists to conveniently secure a parking space within the Central Business District.
+                                With just a few clicks, a user can retrieve information about free spaces available, price of the allocated space and their booking information all through the internet.
+                            </Typography>
+                        </Paper>
+                        {/* </div> */}
                     </Grid>
                 </Grid>
             </Container>
