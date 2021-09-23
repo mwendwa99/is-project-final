3012ceda2cfa00b04a41faa0c6a0cd566cf008dd
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2021-09-23 22:05:48 +0300

    update server side

--- server/routes/spotRoute.js
+++
@@ -1,9 +0,0 @@
-const Spot = require('../model/spotModel');
-const express = require('express');
-const route = express.Router();
-
-route.post('/spot', async (req, res) => {
-    let data = new Spot({
-        const: { name, location, features, spaces, price } = req.body
-    })
-})
